28ae2daf3f09abf813ab80da009aac09b7c1a1d6
BaturaVetal <vetalikbat@gmail.com>
2025-03-08 03:20:40 +0200

    Initial commit for game chat

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let activePlayers = [];
+let gameStarted = false;
+let word = "";
+let currentGuessers = [];
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('join', (nickname) => {
+        console.log(`${nickname} joined the game.`);
+        activePlayers.push({ id: socket.id, nickname: nickname });
+
+        if (activePlayers.length === 1) {
+            socket.emit('checkAdmin', true);
+        } else {
+            socket.emit('checkAdmin', false);
+        }
+    });
+
+    socket.on('startGame', (newWord) => {
+        word = newWord.trim();
+        gameStarted = true;
+        currentGuessers = [];
+        
+        io.emit('gameStarted');
+        io.emit('gameStatus', { gameStarted: true, gameEnded: false });
+        
+        console.log(`Game started with the word: ${word}`);
+    });
+
+    socket.on('chatMessage', (data) => {
+        if (gameStarted && !currentGuessers.includes(data.nickname)) {
+            const isCorrect = data.message.toLowerCase() === word.toLowerCase();
+            if (isCorrect) {
+                io.emit('gameEnded', { winner: data.nickname, word: word });
+                io.emit('gameStatus', { gameStarted: false, gameEnded: true });
+                gameStarted = false;
+                currentGuessers.push(data.nickname);
+                console.log(`${data.nickname} guessed the word!`);
+            }
+            io.emit('chatMessage', { message: data.message, nickname: data.nickname, correct: isCorrect });
+        }
+    });
+
+    socket.on('reconnect', () => {
+        socket.emit('gameStatus', { gameStarted, gameEnded: !gameStarted });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected:', socket.id);
+        activePlayers = activePlayers.filter(player => player.id !== socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
